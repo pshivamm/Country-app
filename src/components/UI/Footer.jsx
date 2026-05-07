c954dbadd9b5dc75227b39a31ac04ea3c39fdd50
pshivamm <shivam.pandey63079801@gmail.com>
2026-05-08 02:40:31 +0530

    style chnages

--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -1,6 +1,6 @@
 export const Footers = () => {
     return (
-        <footer className="bg-white border-b border-gray-300 py-3 px-4">
+        <footer className="bg-gray-100 border-t border-gray-300 py-3 px-4">
             <div className="container">
                 <p className="text-center text-xs text-gray-600">© 2024 Country App. All rights reserved.</p>
             </div>
