8202d6349ab5e28ef07e1550a17f5d92014b1f31
pshivamm <shivam.pandey63079801@gmail.com>
2026-05-08 01:52:00 +0530

    contact form  created

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,11 @@
 export const Contact = () => {
+    const handleFormSubmit = (formData) => {
+        // console.log(formData.entries());
+        const formInputData = Object.fromEntries(formData.entries());
+        console.log(formInputData);
+    }
+
+
     return (
         <section className="hero py-10 md:py-12 px-4">
             <div className="container mx-auto">
@@ -8,6 +15,43 @@ export const Contact = () => {
                 <p className="text-sm text-center leading-sm md:leading-6 text-gray-500 mb-6">
                     Please fill the form below to contact us.
                 </p>
+
+                <div className="max-w-96 mx-auto  bg-white border border-gray-200 rounded-lg p-8">
+                    <form action={handleFormSubmit}>
+                        <input
+                            type="text"
+                            className="mb-3 border border-gray-300 rounded-md w-full px-3 py-3 text-xs text-gray-600 leading-tight focus:outline-red-600"
+                            placeholder="Enter Your Name"
+                            name="username"
+                            autoComplete="false"
+                            required
+                        />
+                        <input
+                            type="email"
+                            className="mb-3 border border-gray-300 rounded-md w-full px-3 py-3 text-xs text-gray-600 leading-tight focus:outline-red-600"
+                            placeholder="Enter Your Email"
+                            name="email"
+                            autoComplete="false"
+                            required
+                        />
+                        <textarea
+                            type="email"
+                            className="border border-gray-300 rounded-md w-full px-3 py-3 text-xs text-gray-600 leading-tight focus:outline-red-600"
+                            rows="3"
+                            placeholder="Enter Your Message"
+                            name="message"
+                            autoComplete="false"
+                            required
+                        ></textarea>
+
+                        <button
+                            className='flex items-center justify-center text-sm md:text-base px-4 py-2 mt-3 rounded-lg bg-blue-500 text-white hover:bg-blue-700 transition duration-600 ease-in-out'
+                            type="submit"
+                            value="send">
+                            Submit
+                        </button>
+                    </form>
+                </div>
             </div>
         </section>
     );
