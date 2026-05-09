b29e196581e18f5d4eb9629b98bc37b2d4155925
pshivamm <shivam.pandey63079801@gmail.com>
2026-05-09 21:50:49 +0530

    country page content

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,3 +1,13 @@
 export const Country = () => {
-    return <h1>Country Page</h1>
+    return (<section className="bg-white py-10 md:py-12 px-4">
+        <div className="container mx-auto">
+            <h2 className="text-2xl font-semibold text-center mb-2 text-blue-900">
+                Country
+            </h2>
+            <p className="text-sm text-center leading-sm md:leading-6 text-gray-500 mb-6">
+                Please fill the form below to contact us.
+            </p>
+        </div>
+    </section>
+    );
 }
